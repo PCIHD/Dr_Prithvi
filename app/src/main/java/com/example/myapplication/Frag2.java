891f3d19a60df5d5205a621c32cc22cd6f65a3e2
Parag Chaudhari <parag.chaudharis@gmail.com>
2019-06-14 16:19:31 +0530

    fixed commit

--- app/src/main/java/com/example/myapplication/Frag2.java
+++ app/src/main/java/com/example/myapplication/Frag2.java
@@ -1,36 +1,85 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.hardware.Camera;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+
+import android.graphics.SurfaceTexture;
+
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.tensorflow.lite.Interpreter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 
 public class Frag2 extends Fragment {
     @Nullable
-    private FrameLayout frameLayout;
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private TextureView frameLayout;
     private BottomSheetBehavior mBottomSheetBehavior;
     private TextView mTextViewState;
     private ImageView Bottom_sheet_arrow;
-    private Camera camera;
+    private CameraDevice cameraDevice;
     private Show_Camera showCamera;
     private View view;
+    private Interpreter tflite;
+    private ImageButton capturebutton;
+    private String cameraId;
+    private CameraCaptureSession cameraCaptureSessions;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private ImageReader imageReader;
+    private Activity activity;
+
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag2_layout , container , false);
+        view = inflater.inflate(R.layout.frag2_layout, container, false);
+        activity = getActivity();
         Bottom_sheet_arrow = (ImageView) view.findViewById(R.id.bottom_sheet_arrow);
-        frameLayout = (FrameLayout) view.findViewById(R.id.camera_frame);
-        mBottomSheetBehavior =  BottomSheetBehavior.from(view.findViewById(R.id.bottom_sheet));
+        frameLayout = (TextureView) view.findViewById(R.id.camera_view);
+        capturebutton = (ImageButton) view.findViewById(R.id.capture);
+        mBottomSheetBehavior = BottomSheetBehavior.from(view.findViewById(R.id.bottom_sheet));
         mBottomSheetBehavior.setBottomSheetCallback(
                 new BottomSheetBehavior.BottomSheetCallback() {
                     @Override
@@ -44,8 +93,7 @@ public class Frag2 extends Fragment {
                                 Bottom_sheet_arrow.setImageResource(R.drawable.icn_chevron_up);
                                 break;
 
-                            case BottomSheetBehavior.STATE_DRAGGING:
-                            {
+                            case BottomSheetBehavior.STATE_DRAGGING: {
                                 Bottom_sheet_arrow.setImageResource(R.drawable.ic_drag_handle_black_24dp);
                             }
                         }
@@ -58,12 +106,38 @@ public class Frag2 extends Fragment {
                     }
                 }
         );
-        camera = Camera.open();
-        Context context = view.getContext();
+        assert frameLayout != null;
+        
+        CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                cameraDevice = camera;
+                createCameraPreview();
+            }
 
-        showCamera = new Show_Camera( context, camera);
-        frameLayout.addView(showCamera);
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraDevice.close();
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+            cameraDevice.close();
+            cameraDevice = null;
+            }
+        };
+
         return view;
+    }
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor fileDescriptor = getContext().getAssets().openFd("dr_prithvi3.tflite");
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLegth = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLegth);
+
     }
 
 
@@ -124,6 +198,61 @@ public class Frag2 extends Fragment {
                 mTextViewState.setText("Sliding...");
             }
         });*/
+
+    private void createCameraPreview() {
+        try {
+            SurfaceTexture texture = frameLayout.getSurfaceTexture();
+            assert texture != null;
+            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
+            Surface surface = new Surface(texture);
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(surface);
+            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    if (cameraDevice == null)
+                        return;
+                    cameraCaptureSessions = session;
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Toast.makeText(getActivity(), "Changed", Toast.LENGTH_SHORT).show();
+
+                }
+            }, null);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+
+        }
+    }
+
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = manager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            assert map != null;
+            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+            //Check realtime permission if run higher API 23
+            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{
+                        Manifest.permission.CAMERA,
+
+                }, REQUEST_CAMERA_PERMISSION);
+                return;
+            }
+        } catch (Exception E1) {
+            E1.printStackTrace();
+        }
+
+
+    }
 }
+
+
+
+
 
 
