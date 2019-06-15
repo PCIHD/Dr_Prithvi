97c66c7e211b7713b68b613070a5ae587026f6eb
Sagarika Verma <sagarikawave@gmail.com>
2019-06-15 05:06:29 -0700

    after tomato

--- app/src/main/java/com/example/myapplication/Frag1.java
+++ app/src/main/java/com/example/myapplication/Frag1.java
@@ -1,39 +1,118 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Frag1 extends Fragment {
+    private LinearLayout listView;
     @Nullable
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         String[] menuItems = {"APPLE_SCAB","APPLE_BLACK_ROT"};
         View view = inflater.inflate(R.layout.frag1_layout , container , false);
-
-        ListView listView = (ListView) view.findViewById(R.id.listview);
-        ArrayAdapter<String> listViewADapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menuItems
-        );
-        listView.setAdapter(listViewADapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Activity activity = getActivity();
+        listView = (LinearLayout)view.findViewById(R.id.listView1);
+        ImageView Tomato = new ImageView(activity);
+        Tomato.setImageResource(R.drawable.tomatoes);
+        Tomato.setContentDescription("Tomato");
+        Tomato.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                }
+            public void onClick(View view) {
+                Intent i= new Intent(getActivity(), tomato.class );
+                startActivity(i);
+
+
+            }
         });
-        return  view;
+        listView.addView(Tomato);
+
+        ImageView Apple = new ImageView(activity);
+        Apple.setImageResource(R.drawable.apple);
+        Apple.setContentDescription("Apple");
+        Apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), apple.class);
+                startActivity(i);
+
+            }
+        });
+        listView.addView(Apple);
+
+        ImageView Blueberry = new ImageView(activity);
+        Blueberry.setImageResource(R.drawable.blueberry);
+        Blueberry.setContentDescription("");
+        listView.addView(Blueberry);
+
+        ImageView Cherry = new ImageView(activity);
+        Cherry.setImageResource(R.drawable.cherry);
+        Cherry.setContentDescription("Cherry");
+        listView.addView(Cherry);
+
+        ImageView Corn = new ImageView(activity);
+        Corn.setImageResource(R.drawable.corn);
+        Corn.setContentDescription("Corn");
+        listView.addView(Corn);
+
+        ImageView Grapes = new ImageView(activity);
+        Grapes.setImageResource(R.drawable.grapes);
+        Grapes.setContentDescription("Grapes");
+        listView.addView(Grapes);
+
+        ImageView Orange = new ImageView(activity);
+        Orange.setImageResource(R.drawable.orange);
+        Orange.setContentDescription("Orange");
+        listView.addView(Orange);
+
+/*
+
+
+
+
+        // Define string array.
+        String[] listValue = new String[] {"ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT"};
+
+        @Override
+        /*protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main); */
+
+/*
+
+
+
+            // ListView on item selected listener.
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+            {
+
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    // TODO Auto-generated method stub
+
+                    // Getting listview click value into String variable.
+                    String TempListViewClickedValue = listValue[position].toString();
+
+                    Intent intent = new Intent(activity,ag1_second.class);
+
+                    // Sending value to another activity using intent.
+                    intent.putExtra("ListViewClickedValue", TempListViewClickedValue);
+
+                    startActivity(intent);
+
+                }
+            });
+            */
+        return view;
     }
 }
