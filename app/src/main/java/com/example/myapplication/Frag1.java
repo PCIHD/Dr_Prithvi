cb5a9e52c166c76035701a4697156f63614f027a
Shivani Dere <shivanidere@outlook.com>
2019-06-15 12:33:45 +0530

    frag 1 update

--- app/src/main/java/com/example/myapplication/Frag1.java
+++ app/src/main/java/com/example/myapplication/Frag1.java
@@ -1,18 +1,39 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Frag1 extends Fragment {
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        String[] menuItems = {"APPLE_SCAB","APPLE_BLACK_ROT"};
         View view = inflater.inflate(R.layout.frag1_layout , container , false);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview);
+        ArrayAdapter<String> listViewADapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                menuItems
+        );
+        listView.setAdapter(listViewADapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                }
+        });
         return  view;
     }
 }
