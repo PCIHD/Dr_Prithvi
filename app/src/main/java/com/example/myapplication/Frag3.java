bd65f773a11ab6c593ae2fa0e1c791bef8d85f4e
Shivani Dere <shivanidere@outlook.com>
2019-06-15 20:54:52 +0530

    frag3

--- app/src/main/java/com/example/myapplication/Frag3.java
+++ app/src/main/java/com/example/myapplication/Frag3.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +9,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Frag3 extends Fragment {
+    private LinearLayout listView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag3_layout , container , false);
+        Activity activity = getActivity();
+        listView = (LinearLayout)view.findViewById(R.id.listView1);
+        TextView Scheme1 = new TextView(activity);
+        Scheme1.setContentDescription("Scheme1");
+        Scheme1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getActivity(), scheme1.class );
+                startActivity(i);
+
+
+      }
+});
+
+
+
         return view;
     }
 }
