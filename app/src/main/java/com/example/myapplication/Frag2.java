53d5dc4c3e44886a1b5fd4201e4444b60fc122c1
Parag Chaudhari <parag.chaudharis@gmail.com>
2019-10-05 10:04:49 +0530

    working model accuracy issues

--- app/src/main/java/com/example/myapplication/Frag2.java
+++ app/src/main/java/com/example/myapplication/Frag2.java
@@ -89,7 +89,7 @@ public class Frag2 extends Fragment {
     private int intValues[];
     private static final int RESULTS_TO_SHOW = 3;
     private float[][] labelProbArray = null;
-    private static final String TF_OD_API_MODEL_FILE = "dr_prithvi3.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "dr_prithvi4.tflite";
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/label.txt";
 
 
@@ -235,61 +235,6 @@ public class Frag2 extends Fragment {
 
 
 
-   /* @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag1_layout);
-
-        View bottomSheet = findViewById(R.id.bottom_sheet);
-
-        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-
-        mTextViewState = findViewById(R.id.text_view_state);
-
-        Button buttonExpand = findViewById(R.id.button_expand);
-        Button buttonCollapse = findViewById(R.id.button_collapse);
-
-        buttonExpand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-            }
-        });
-
-        buttonCollapse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-            }
-        });
-
-        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        mTextViewState.setText("Collapsed");
-                        break;
-                    case BottomSheetBehavior.STATE_DRAGGING:
-                        mTextViewState.setText("Dragging...");
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        mTextViewState.setText("Expanded");
-                        break;
-                    case BottomSheetBehavior.STATE_HIDDEN:
-                        mTextViewState.setText("Hidden");
-                        break;
-                    case BottomSheetBehavior.STATE_SETTLING:
-                        mTextViewState.setText("Settling...");
-                        break;
-                }
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                mTextViewState.setText("Sliding...");
-            }
-        });*/
 
 
     }
